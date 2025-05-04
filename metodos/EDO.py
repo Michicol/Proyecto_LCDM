27b582e923ac85fa883124608691b3a6324c4c47
Michicol <lmichicol7@gmail.com>
2025-05-03 23:35:13 -0600

    Acutalizacion 03-05-25 11:35

--- metodos/EDO.py
+++ metodos/EDO.py
@@ -34,17 +34,21 @@ class EDO:
             Y[i + 1] = Y[i] + h / 2 * (k1 + 2 * k2)
         return Y
 
-    def rk4Solve(X, F, U0, **args):
+    def rk4Solve(dYdX, Y0, X, args):
+        """
+        dYdX: Derivada de la Función
+        Y0: float valor inicial
+        X: arr dominio
+        args: posibles parametros que requiera la derivada
+        """
         n = len(X)
-        h = (X[n - 1] - X[0]) / n
-        U = np.zeros((n, len(U0)))
-
-        U[0] = U0
-
-        for i in range(0, n - 1):
-            k1 = F(U[i], **args)
-            k2 = F(U[i] + k1 * h / 2, X[i], args)
-            k3 = F(U[i] + k2 * h / 2, X[i], args)
-            k4 = F(U[i] + k3 * h, args, X[i], args)
-            U[i + 1] = U[i] + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
-        return U
+        h = X[1] - X[0]  # Paso de integración
+        Y = np.zeros((n, len(Y0)))
+        Y[0] = Y0
+        for i in range(n - 1):
+            k1 = dYdX(Y[i], X[i], args)
+            k2 = dYdX(Y[i] + k1 * h / 2.0, X[i] + h / 2.0, args)
+            k3 = dYdX(Y[i] + k2 * h / 2.0, X[i] + h / 2.0, args)
+            k4 = dYdX(Y[i] + k3 * h, X[i] + h, args)
+            Y[i + 1] = Y[i] + (h / 6.0) * (k1 + 2 * k2 + 2 * k3 + k4)
+        return Y
